678933039cfb59551afeb2f3aba246a7bcd6bff7
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-07-04 09:23:59 +0700

    Page mock with specific page data

--- resource/mock/page.js
+++ resource/mock/page.js
@@ -2,9 +2,12 @@ module.exports = function (page) {
   if (!page) return null;
 
   return {
-    'page_id': page._id,
-    'page_url': page.page_url,
-    'page_title': page.page_title,
-    'variant': page.variant
+    'page_id': page.page._id,
+    'page_url': page.page.page_url,
+    'page_title': page.page.page_title,
+    'theme_id': page.page.theme,
+    'variant': page.page.variant,
+    'role_id': page.role,
+    'role_group': page.role_group
   };
 }
