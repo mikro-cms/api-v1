1753fbbfeb5c5877c489ea2244d7330b5727b5b0
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-07-10 09:25:40 +0700

    Mock page have two condition using theme id or theme object

--- resource/mock/page.js
+++ resource/mock/page.js
@@ -1,13 +1,30 @@
+const mockTheme = require('./theme');
+
 module.exports = function (page) {
   if (!page) return null;
 
-  return {
+  let mockedPage = {
     'page_id': page.page._id,
     'page_url': page.page.page_url,
     'page_title': page.page.page_title,
-    'theme_id': page.page.theme,
     'variant': page.page.variant,
     'role_id': page.role,
     'role_group': page.role_group
   };
+
+  if (typeof page.page.theme === 'object') {
+    let mockedTheme = mockTheme(page.page.theme);
+
+    mockedPage = {
+      ...mockedPage,
+      ...mockedTheme
+    };
+  } else {
+    mockedPage = {
+      ...mockedPage,
+      'theme_id': page.theme
+    };
+  }
+
+  return mockedPage;
 }
